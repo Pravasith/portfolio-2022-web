36110af66aa735918f1901fb3d49b54c9490a36c
Pravasith <pravasith@Abhijeets-MacBook-Pro.local>
2022-02-28 22:03:55 -0500

    fixed next linting 💥🗾

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    eslint: {
+        dirs: ["."], //or ['pages', 'hooks']
+    },
 }
 
 module.exports = nextConfig
